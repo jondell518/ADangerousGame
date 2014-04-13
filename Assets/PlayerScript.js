922a80aeecd1f03eb4c57574e84b2836d0a8400e
jondell518 <jondell518@gmail.com>
2014-04-12 22:53:51 -0400

    Networking now works for two players concurrently

---
+++ Assets/PlayerScript.js
@@ -0,0 +1,22 @@
+﻿var speed: int = 5;
+var gravity = 5;
+
+
+private var cc:CharacterController;
+
+function Start () {
+
+	cc = GetComponent(CharacterController);
+}
+
+function Update () {
+
+	if(networkView.isMine){
+		cc.Move(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, -gravity * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime));
+	}
+	
+
+
+
+
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-ADangerousGame
-==============
-
-Final Project for Game Development CSC-589 American University Spring 2014 
